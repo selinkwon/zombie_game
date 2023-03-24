2a9b15772c2614d8f843dd000a68f4d8b88ca95c
tpfls123v <tpfls123v@naver.com>
2023-03-24 11:11:43 +0900

    Create zombie_game

---
+++ zombie_game/src/zombie_game/Hero.java
@@ -0,0 +1,5 @@
+package zombie_game;
+
+public class Hero {
+
+}
---
+++ zombie_game/src/zombie_game/zombiegame.java
@@ -0,0 +1,10 @@
+package zombie_game;
+
+public class zombiegame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
